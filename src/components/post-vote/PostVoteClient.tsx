e023f7793636e542f7fda79c9247c524bbf322af
Schlumen <lumen.haendler@web.de>
2025-01-22 16:03:00 +0100

    Show posts

---
+++ src/components/post-vote/PostVoteClient.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { usePrevious } from "@mantine/hooks";
+import { VoteType } from "@prisma/client";
+import { FC, useEffect, useState } from "react";
+import { Button } from "../ui/Button";
+import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface PostVoteClientProps {
+  postId: string;
+  initailVotesAmt: number;
+  initialVote?: VoteType | null;
+}
+
+const PostVoteClient: FC<PostVoteClientProps> = ({
+  postId,
+  initailVotesAmt,
+  initialVote,
+}) => {
+  const { loginToast } = useCustomToast();
+  const [votesAmt, setVotesAmt] = useState<number>(initailVotesAmt);
+  const [currentVote, setCurrentVote] = useState(initialVote);
+  const prevVote = usePrevious(currentVote);
+
+  useEffect(() => {
+    setCurrentVote(initialVote);
+  }, [initialVote]);
+
+  return (
+    <div className="flex flex-col gap-0 pr-6 sm:w-20 pb-0">
+      <Button size="sm" variant="ghost" aria-label="upvote">
+        <ArrowBigUp
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-emerald-500 fill-emerald-500": currentVote === "UP",
+          })}
+        />
+      </Button>
+
+      <p className="text-center py-2 font-medium text-sm text-zinc-900">
+        {votesAmt}
+      </p>
+
+      <Button size="sm" variant="ghost" aria-label="upvote">
+        <ArrowBigDown
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-red-500 fill-red-500": currentVote === "DOWN",
+          })}
+        />
+      </Button>
+    </div>
+  );
+};
+
+export default PostVoteClient;
